1ae095aaee6d93cc41963783d341e7909afd108d
Harikrishnan <haripdm9446718962@gmail.com>
2024-01-24 19:24:09 +0530

    something error

---
+++ broenv/Scripts/hello_django/app_one/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class MovieInfo(models.Model):
+    title = models.CharField(max_length = 20)
+    year = models.IntegerField()
+    description = models.TextField()
+    poster = models.ImageField(upload_to='media/',null=True)
+
+    def __str__(self) :
+        return self.title
---
+++ broenv/Scripts/hello_django/app_one/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render
+from .models import MovieInfo
+from .forms import MovieForm
+
+# Create your views here.
+
+
+def list(request):
+    movie_set = MovieInfo.objects.all()
+
+    return render(request,'list.html',{'mov':movie_set})
+
+def edit(request,pk):
+    instance_to_be_edited = MovieInfo.objects.get(pk=pk)
+    if request.POST:
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+        description = request.POST.get('description')
+        instance_to_be_edited.title = title
+        instance_to_be_edited.year = year
+        instance_to_be_edited.description = description
+        instance_to_be_edited.save()
+
+        
+        movie_set = MovieInfo.objects.all()
+
+        return render(request,'list.html',{'mov':movie_set})
+
+
+
+
+    frm = MovieForm(instance=instance_to_be_edited)
+
+    return render(request,'create.html',{'frm':frm})
+
+def home(request):
+    sample ={
+        'list': [
+
+            'god_father.jpg',
+            'aadu_1.webp',
+            'god_father.jpg'
+
+
+
+        ]
+    }
+    return render(request,'home.html',sample)
+
+def movies(request):
+    mov_dict = {
+        'items':[
+           {
+        'title':'godfather',
+        'summary':'story of an underworld king',
+        'year':'2003',
+        'sucess':False,
+        'img':'god_father.jpg'
+        },
+        {
+        'title':'titanic',
+        'summary':'story of an underworld king',
+        'year':'2003',
+        'sucess':True,
+        'img':'titanic.jpg'
+        },
+        {
+        'title':'aadu',
+        'summary':'',
+        'year':'2003',
+        'sucess':True,
+        'img':'aadu_1.webp'
+        } 
+        ]
+    }
+    return render(request,'edit.html',mov_dict)
+    
+
+def create(request):
+    
+    frm = MovieForm()
+    if request.POST:
+        """
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+        description = request.POST.get('description')  
+        MovieInfo_obj = MovieInfo(title=title,year=year,description=description)
+        MovieInfo_obj.save()   """
+        frm = MovieForm(request.POST)
+        if frm.is_valid():
+            frm.save()
+
+        else:
+            frm = MovieForm()
+
+    
+    return render(request,'create.html',{'frm':frm})  
+
+def delete(request,pk):
+    instance = MovieInfo.objects.get(pk=pk)
+    instance.delete()
+    movie_set = MovieInfo.objects.all()
+    return render(request,'list.html',{'mov':movie_set})  
+
+
+
