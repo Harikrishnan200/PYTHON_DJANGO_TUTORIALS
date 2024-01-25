33c931e187ae866c6c4f52bf3265b8e9e7f67a0c
Harikrishnan <haripdm9446718962@gmail.com>
2024-01-25 19:17:00 +0530

    orm relations completed

--- broenv/Scripts/hello_django/app_one/models.py
+++ broenv/Scripts/hello_django/app_one/models.py
@@ -7,20 +7,31 @@ class CensorInfo(models.Model):
     certified_by = models.CharField(max_length = 200,null = True)  
 
 
+class Director(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+    
+class Actor(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+
 class MovieInfo(models.Model):
     title = models.CharField(max_length = 20)
     year = models.IntegerField()
     description = models.TextField()
     poster = models.ImageField(upload_to='media/',null=True)
     censor_details = models.OneToOneField(CensorInfo,on_delete = models.SET_NULL,related_name = 'movies',null = True)
+    directed_by = models.ForeignKey(Director,null = True,on_delete = models.CASCADE,related_name = 'directed_movies')
+    actors = models.ManyToManyField(Actor,related_name='acted_movies')
 
     def __str__(self) :
         return self.title
     
-class Director(models.Model):
-    name = models.CharField(max_length = 20)
-
-    def __str__(self):
-        return self.name    
+    
     
 
