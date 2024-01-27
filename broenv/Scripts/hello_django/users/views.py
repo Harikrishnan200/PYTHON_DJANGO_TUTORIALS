c2a186d9a7fe3cc0858c430109a2ae379484146b
Harikrishnan <haripdm9446718962@gmail.com>
2024-01-27 10:55:32 +0530

    sign up completed

--- broenv/Scripts/hello_django/users/views.py
+++ broenv/Scripts/hello_django/users/views.py
@@ -4,10 +4,16 @@ from django.contrib.auth.models import User
 # Create your views here.
 def signUp(request):
     user = None
+    error_message = None
     if request.POST:
+        print('inside if')
+        
         username = request.POST['username']
         password = request.POST['password']
         print(username,password)
-       # user = User.objects.create_user(username=username,password=password)
-
-    return render(request,'signup.html',{'user_obj':user})
+        try:
+            user = User.objects.create_user(username=username,password=password)
+        except Exception as e:
+            error_message = str(e)
+    
+    return render(request,'signup.html',{'user':user,'error_msg':error_message})
